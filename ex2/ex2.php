5e04f94c88a1cd51436b313f2f8b38cf4533ccb9
victoriasgit <vikagina@yandex.ru>
2018-03-05 15:55:00 +0300

    new ex2

--- ex2/ex2.php
+++ ex2/ex2.php
@@ -1,71 +1,44 @@
 <?php
 
-function translation($oldString) {
-    $newString = '';
-    $count = 0;   //количество изменений
-    foreach(str_split($oldString) as $key => $symbol) {
-        switch ($symbol) {
-            case "h":
-                $newString .= '4';
-                $count++;
-                break;
-            case "l":
-                $newString .= '1';
-                $count++;
-                break;
-            case "e":
-                $newString .= '3';
-                $count++;
-                break;
-            case "o":
-                $newString .= '0';
-                $count++;
-                break;
-            default:
-                $newString .= $symbol;
-                break;
+$count = 0 ;
+
+function translation($oldString, &$count) : string {
+
+    function gen($oldString, &$count)  {
+
+        $length = strlen($oldString);      //длина введенной строки
+
+        for ($i = 0; $i < $length; $i++) {    //перебираем символы, если находим нужный - делаем замену и увеличиваем переменную count
+            switch ($oldString[$i]) {
+                case "h":
+                    yield "4";
+                    $count++;
+                    break;
+                case "l":
+                    yield "1";
+                    $count++;
+                    break;
+                case "e":
+                    yield "3";
+                    $count++;
+                    break;
+                case "o":
+                    yield "0";
+                    $count++;
+                    break;
+                default:
+                    yield $oldString[$i];
+                    break;
+            }
         }
+        return [$oldString, $count];
     }
 
-    return [$newString, $count];
-
-//    function gen($oldString)  {
-//        $count = 0;
-//        $length = strlen($oldString);      //длина введенной строки
-//
-//        for ($i = 0; $i < $length; $i++) {    //перебираем символы, если находим нужный - делаем замену и увеличиваем переменную count
-//            switch ($oldString[$i]) {
-//                case "h":
-//                    yield "4";
-//                    $count++;
-//                    break;
-//                case "l":
-//                    yield "1";
-//                    $count++;
-//                    break;
-//                case "e":
-//                    yield "3";
-//                    $count++;
-//                    break;
-//                case "o":
-//                    yield "0";
-//                    $count++;
-//                    break;
-//                default:
-//                    yield $oldString[$i];
-//                    break;
-//            }
-//        }
-//        return $count;
-//    }
-//
-//    $generator = gen($oldString);   //генератор
-//
-//    $newString = "";
-//    foreach ($generator as $i) {         //составляем новую строку из символов
-//        $newString = $newString . $i;
-//    }
-//    return $newString;
+    $newString = "";
+    foreach (gen($oldString, $count) as $i) {         //составляем новую строку из символов
+        $newString = $newString . $i;
+    }
+    return $newString;
 
 }
 
@@ -74,10 +47,6 @@ if (isset($_POST['text'])) {
     $input = $_POST['text'];
 }
 
-//echo "Новая строка: " . translation($input);
-
-echo "Новая строка: ";
-list($newString, $count) = translation($input);
-var_export($newString);
+echo "Новая строка: " . translation($input, $count);
 echo "<br/> Число замен: ";
 var_export($count);
